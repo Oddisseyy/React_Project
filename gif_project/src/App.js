a405856c4e09c2ab74af064a36eeaac0cefebd1d
asanchez <andreucas1997@gmail.com>
2022-08-01 14:57:42 +0200

    modify app files

--- gif_project/src/App.js
+++ gif_project/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+const GIFS = ['https://media3.giphy.com/media/xT9KVD5I2pKy21DY40/giphy.gif?cid=ecf05e47r87wvzaok1b8kg8w36wn3ghx03y2zoo4cg0yi96w&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/10UUe8ZsLnaqwo/giphy.gif?cid=ecf05e47ggd6zqdzeuamm0kj3yg5ioz2am2ph89sml7dezwc&rid=giphy.gif&ct=g'];
+
 function App() {
+  
+  const [gifs, setGifs] = useState(GIFS);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <img src = {gifs} alt=''/>
+      </section>
     </div>
   );
 }
